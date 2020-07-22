65973a280939bacbf1c95e812a17b90b7921ca4a
edwinisnow <edwin04@gmail.com>
2020-07-22 15:33:36 +0100

    React ToolTip Demo

--- src/App.js
+++ src/App.js
@@ -1,33 +1,43 @@
-import React, { useState } from 'react';
+import React, { forwardRef } from 'react';
 import './App.css';
-import Modal from 'react-modal'
+import Tippy from '@tippy.js/react';
+import 'tippy.js/dist/tippy.css';
 
-Modal.setAppElement('#root')
+const ColoredToolTip = () => {
+  return <span style={{ color: 'yellow' }}>Colored Component</span>
+}
+const CustomChild = forwardRef((props, ref) => {
+  return <div ref={ref}>
+    <div>First Line</div>
+    <div>Second Line</div>
+  </div>
+})
+
 
 function App() {
-  const [modalIsOpen, setModalIsOpen] = useState(false)
 
   return (
-    <div>
-      <button onClick={() => setModalIsOpen(true)} >Open</button>
-      <Modal
-        isOpen={modalIsOpen}
-        onRequestClose={() => setModalIsOpen(false)}
-        style={
-          {
-            overlay: {
-              backgroundColor: 'grey'
-            },
-            content: {
-              color: 'orange'
-            }
-          }
-        }>
-        <h2>Modal Title</h2>
-        <p>Modal Body</p>
-        <div> <button onClick={() => setModalIsOpen(false)} >Close</button></div>
-      </Modal>
-
+    <div >
+      <div style={{ paddingBottom: '20px' }}>
+        <Tippy arrow={false} delay={1000} placement='right' content="Basic Tooltip">
+          <button>Hover</button>
+        </Tippy>
+      </div>
+      <div style={{ paddingBottom: '20px' }}>
+        <Tippy content={<span style={{ color: 'orange' }}>Colored</span>}>
+          <button>Hover</button>
+        </Tippy>
+      </div>
+      <div style={{ paddingBottom: '20px' }}>
+        <Tippy content={<ColoredToolTip />}>
+          <button>Hover</button>
+        </Tippy>
+      </div>
+      <div style={{ paddingBottom: '20px' }}>
+        <Tippy content={<ColoredToolTip />}>
+          <CustomChild />
+        </Tippy>
+      </div>
     </div>
   );
 }
